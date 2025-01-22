af2b80d035f4cb6f2ea6c5715a8531e54d74d751
OlegGlo <olegglotov99@gmail.com>
2025-01-22 15:32:36 -0500

    added cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # pkgs
 from flask import Flask
 from waitress import serve
+from flask_cors import CORS
 
 # modules
 from api.image_routes import image_blueprint
@@ -8,6 +9,7 @@ from api.image_routes import image_blueprint
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "https://orbitwatch.xyz"}})
     app.register_blueprint(image_blueprint)
     return app
 
