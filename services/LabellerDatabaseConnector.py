54949372e9c89ef0d6cfbcd9610c28a554da8ad7
Kyle McMaster <67760150+KyleJMcMaster@users.noreply.github.com>
2025-03-19 15:54:10 -0400

    Update LabellerDatabaseConnector.py

--- services/LabellerDatabaseConnector.py
+++ services/LabellerDatabaseConnector.py
@@ -143,7 +143,7 @@ class MYSQLLabellerDatabaseConnector(LabellerDatabaseConnector):
                 result = connection.execute(text(query), data)
                 print(f"Query returned {result.rowcount} results") 
                 for res in result:
-                    results.append({'profile_picture': str(res[2]),
+                    results.append({'profile_picture': base64.b64encode(user['profile_picture']).decode('utf-8') if user['profile_picture'] else None,
                                     'first_name': res[3],
                                      'creation_date': str(res[7])})
                 return results
