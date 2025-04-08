9c3cbf60007122f99273b72c4c939fca49f0cd23
OlegGlo <olegglotov99@gmail.com>
2025-04-08 10:26:22 -0400

    cors upd

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ from api.account_routes import user_project_blueprint
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": ["https://orbitwatch.xyz", "http://localhost:3000"]}})
+    CORS(app, resources={r"/api/*": {"origins": [
+        "https://orbitwatch.xyz", 
+        "http://localhost:3000",
+        r"^https://deploy-preview-\d+--okkm\.netlify\.app$"
+        ]}})
     app.register_blueprint(image_blueprint)
     app.register_blueprint(user_project_blueprint)
     print("launched")
